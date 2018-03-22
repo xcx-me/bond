2b3508a40bebfd401a347c0c3eac321b54e930cd
Anthony Chen <flashcgd@sina.com>
2018-03-22 11:37:05 +0800

    Remove useless code.

--- app/util/ajax/ajax.js
+++ app/util/ajax/ajax.js
@@ -1,8 +1,6 @@
 const Promise = require('./es6-promise').Promise
 
 function request (config, data) {
-	console.log('config', config)
-
 	return new Promise((resolve, reject, ajaxHandle) => {
 		wx.request({
 			url: config.url,
