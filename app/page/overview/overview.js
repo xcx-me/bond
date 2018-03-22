b6eafcf39ba48e61846025e6394d7cd68f5ef0f7
rita <hf@appletreetech>
2018-03-22 18:09:36 +0800

    add dynamic component

--- app/page/overview/overview.js
+++ app/page/overview/overview.js
@@ -1,4 +1,6 @@
 // app/page/overview/overview.js
+const { request } = require('../../util/ajax/ajax')
+const config = require('../../util/ajax/config')
 
 Page({
   /**
@@ -13,8 +15,23 @@ Page({
 		onsale_bond: "0", 
 		click_num: "0", 
 		share_num: "0"
-	}
+	},
+	daynamicList:  []
   },
+
+  doRequest: function () {
+	request(config.EXAMPLE.getSometing, {}).then((result) => {
+		wx.showToast({
+			title: '请求成功',
+			icon: 'success',
+			mask: true,
+			duration: 2000
+		})
+		console.log('request success', result)
+	}).catch((error) => {
+		// Do something...
+	})
+},
 
   getStoreDetail: function () {
     this.setData({
@@ -27,6 +44,17 @@ Page({
     })
   },
 
+  getStoreDynamics: function () {
+	this.setData({
+		daynamicList: [
+			{bond_simple_name: "22", bond_id: "2535a86e7b0225cb0345d1afd33e1483"},
+			{bond_simple_name: "22LKI", bond_id: "b1f3e6f3188feb7c5466070bc727e2a5"},
+			{bond_simple_name: "22i", bond_id: "8ce7a3977c1fafd128c1af75fdac8e72"},
+			{bond_simple_name: "债券简称1", bond_id: "36b5969fe0ef5a153a1121cb9e194908"}
+		]
+	})
+  },
+
   checkboxChange: function (e) {
 	this.setData({
 		isAgreedQtrade: e.detail.value.length > 0
@@ -37,7 +65,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getStoreDetail()
+	this.getStoreDetail()
+	if (this.data.isAgreedQtrade) {
+		this.getStoreDynamics()
+	}
   },
 
   /**
--- app/util/ajax/config.js
+++ app/util/ajax/config.js
@@ -9,5 +9,101 @@ module.exports = {
 	},
 	OTHER_SERVICE: {
 		
-	}
+	},
+	NEW_BOND: { // 新债
+		// 发布报价相关
+		sendBond: { // 发布报价
+			url: '/qtrade_bond/api/newbond/quotation.do',
+			method: POST
+		},
+		associateBondName: { // 债券简称联想
+			url: '/qtrade_bond/api/newbond/associate_bond_name.do',
+			method: POST
+		},
+		associateBond: { // 债券信息联想
+			url: '/qtrade_bond/api/newbond/associate.do',
+			method: POST
+		},
+		judgeShopOpen: { // 是否开通店铺
+			url: '/qtrade_bond/api/newbond/is_myshop_opened.do',
+			method: POST
+		},
+		openMyShop: { // 去开通点店铺
+			url: '/qtrade_bond/api/newbond/open_myshop.do',
+			method: POST
+		},
+		// ==========
+
+		getWorkdaysInfo: { // 发行面板上工作日列表返回
+			url: '/qtrade_bond/api/newbond/get_workdays_info.do',
+			method: POST
+		},
+		consultation: { // 询量
+			// url: '/appletree/unreadmsglist.do',
+			url: '/qtrade_bond/api/newbond/quotation_board.do',
+			method: POST
+		},
+		announcement: { // 公告
+			// url: '/appletree/unreadmsglist.do',
+			url: '/qtrade_bond/api/newbond/quotation_board.do',
+			method: POST
+		},
+		issue: { // 发行
+			// url: '/appletree/unreadmsglist.do',
+			url: '/qtrade_bond/api/newbond/quotation_board.do',
+			method: POST
+		},
+		accumulateClick: { // 点击次数累加
+			url: '/qtrade_bond/api/newbond/accumulate_click.do',
+			method: POST
+		},
+		isStoreOpened: { // 店铺是否开通接口
+			url: '/qtrade_bond/api/newbond/is_myshop_opened.do', // /qtrade_bond/api/is_myshop_opened.do
+			method: POST
+		},
+		news: { // 我的店铺--查询最新动态
+			url: '/qtrade_bond/api/newbond/get_dynamic_tips.do',
+			method: POST
+		},
+		deleteNews: { // 我的店铺--点击最新动态
+			url: '/qtrade_bond/api/newbond/delete_dynamic_tip.do',
+			method: POST
+		},
+		newBondList: { // 债券列表
+			url: '/qtrade_bond/api/newbond/get_bond_list.do',
+			method: POST
+		},
+		deleteBond: { // 删除债券
+			url: '/qtrade_bond/api/newbond/delete_bond.do',
+			method: POST
+		},
+		storeDetail: { // 店铺详情
+			url: '/qtrade_bond/api/newbond/get_shop_detail.do',
+			method: POST
+		},
+		modNewBondDetail: {
+			url: '/qtrade_bond/api/newbond/mod_quotation.do',
+			method: POST
+		},
+		newBondDetail: { // 债券详情
+			url: '/qtrade_bond/api/newbond/get_bond_detail.do',
+			method: POST
+		},
+		questionQuery: { // 问答区查询
+			url: '/qtrade_bond/api/newbond/query_asklist.do', // /qtrade_bond/api/query_asklist.do
+			method: POST
+		},
+		askQuestion: { // 问问题
+			url: '/qtrade_bond/api/newbond/ask.do',
+			method: POST
+		},
+		answerQuestion: { // 回答问题
+			url: '/qtrade_bond/api/newbond/ans.do',
+			method: POST
+		},
+		thumbQuestion: { // 点赞
+			url: '/qtrade_bond/api/newbond/thumb.do',
+			method: POST
+		}
+	},
 }
