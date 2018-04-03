cc1379a4f4f17d2afe235bfe8ec1544359421843
sunqing <liusunqing@aliyun.com>
2018-04-03 19:04:08 +0800

    bond-create-tool

--- app/ui/bond-form-editor/bond-form-editor.js
+++ app/ui/bond-form-editor/bond-form-editor.js
@@ -1,3 +1,8 @@
+
+var dateTimePicker = require('../../util/date-time-picker/date-time-picker')
+
+const selectConfig = require('./select-config/select-config')
+
 Component({
 	properties: {
 		editorFlag: { // 是否是编辑债券的入口
@@ -9,23 +14,58 @@ Component({
 
 	data: {
 		formData: {
-			bondName: '',
-			subjectRate: '',
-			facilityRate: '',
-			rateMethod: '',
-			benchMark: '',
-			circulation: '',
-			releaseTime: ''
+			bond_simple_name: '', // 简称
+			left_benefit: '', // 参考收益左值
+			right_benefit: '', // 参考收益右值
+			subject_rating: '', // 主体评级
+			facility_rating: '', // 债项评级
+			deadline: '', // 期限
+			issue_total: '', // 发行量
+			public_place: '', // 上市地点
+			issue_time: '', // 发行时间
+			bid_end: '', // 截标时间
+			company_type: '', // 企业性质
+			bond_type: '', // 债券品种
+			issue_way: '', // 发行方式
+			rate_way: '', // 利率方式
+			benchmark: '', // 基准
+			cal_freq: '', // 计息频率
+			pay_freq: '', // 付息频率
+			repay_way: '', // 还本方式
+			specific_items: '', // 特殊条款
+			credit_guarantee: '', // 增信担保 
+			zhu_cheng: '', // 主承
+			bond_full_name: '', // 债券全称
+			attached_ids: '' 
 		},
-		// date: '',
+		// 时间
+		dateTimeArray: dateTimePicker.dateTimePicker(2000, 2050).dateTimeArray,
+		dateTime: dateTimePicker.dateTimePicker(2000, 2050).dateTime,
 
-		// 这里是一些组件内部数据
-		text: "text",
+		// 上市地点 多选框
+		listingSpotItems: selectConfig.listingSpot.items, 
+		listingSpotValue: '',
+
+		// 企业性质
+		enterpriseNature: selectConfig.enterpriseNature.items, 
+		enterpriseIndex: -1,
+
+		// 债券品种
+		bondType: selectConfig.bondType.items,
+		bondTypeValue: '',
+
+		// 发行方式
+		issuanceMethodItem: selectConfig.issuanceMethod.items,
+		issuanceMethodIndex: -1,
 
 		//  利率方式
-		rateWayArray: ['利随本清', '固定利率', '浮动利率', '累积利率'],
+		rateWayArray: selectConfig.rateWay.items,
 		rateWayIndex: -1,
-		rateReply: false
+		rateReply: false,
+
+		// 特殊条款
+		specificItems: selectConfig.specificClause.items,
+		specificitemsValue: ''
 	},
 
 	methods: {
@@ -38,15 +78,73 @@ Component({
 			this.triggerEvent('changeValueEvent', formData)
 		},
 
-		// bindDateChange: function(e) {
-		// 	console.log('picker发送选择改变，携带值为', e.detail.value)
-		// 	// let formData = this.data.formData
-		// 	// formData[e.currentTarget.dataset.inputName] = e.detail.value
-		// 	this.setData({
-		// 		date: e.detail.value
-		// 	})
-		// },
+		// picker,年月日 + 时分
+		changeDateTime (e) {
+			let formData = this.data.formData
+			formData[e.currentTarget.dataset.inputName] = this.data.dateTimeArray[0][e.detail.value[0]] +'-'+ this.data.dateTimeArray[1][e.detail.value[1]] +'-'+ this.data.dateTimeArray[2][e.detail.value[2]] +' '+ this.data.dateTimeArray[3][e.detail.value[3]] +':'+ this.data.dateTimeArray[4][e.detail.value[4]]
+			this.setData({
+				dateTime: e.detail.value
+			});
+			// console.log(this.data.formData.closingTime)
+		},
 
+		// 多选下拉框
+		checkboxChange: function (e) {
+			let checkedValue = e.detail.value
+			let checkedItem = []
+			let checkedName = []
+			checkedValue.forEach((item, index) => {
+				checkedItem.push(this.data.listingSpotItems.find((items) => { return items.name === item}).value)
+				checkedName.push(item)
+			})
+
+			let formData = this.data.formData
+			formData[e.currentTarget.dataset.listingSpot] = checkedName.join('|')
+			this.setData({
+				listingSpotValue: checkedItem.join('、'),
+				formData: formData
+			})
+		},
+
+		// 企业性质
+		changeEnterpriseNature: function (e) {
+			// console.log('---enterpriseNature: ', this.data.enterpriseNature[e.detail.value])
+			let formData = this.data.formData
+			formData[e.currentTarget.dataset.enterpriseNature] = Number(e.detail.value) + 1
+			this.setData({
+				enterpriseIndex: e.detail.value
+			})
+		},
+
+		// 债券品种
+		changeBondType: function (e) {
+			let checkedValue = e.detail.value
+			let checkedItem = []
+			let checkedName = []
+			checkedValue.forEach((item, index) => {
+				checkedItem.push(this.data.bondType.find((items) => { return items.name === item}).value)
+				checkedName.push(item)
+			})
+
+			let formData = this.data.formData
+			formData[e.currentTarget.dataset.bondType] = checkedName.join('|')
+			this.setData({
+				bondTypeValue: checkedItem.join('、'),
+				formData: formData
+			})
+		},
+
+		// 发行方式
+		changeIssuancemethod: function (e) {
+			let formData = this.data.formData
+			formData[e.currentTarget.dataset.issuanceMethod] = Number(e.detail.value) + 1
+			this.setData({
+				issuanceMethodIndex: e.detail.value,
+				formData: formData
+			})
+		},
+
+		// 利率方式
 		handleDropdownRateWay: function (e) {
 			console.log('----checked: ', this.data.rateWayArray[e.detail.value])
 			// wx.showToast({
@@ -60,11 +158,29 @@ Component({
 				this.setData({ rateReply: false })
 			}
 			let formData = this.data.formData
-			formData.rateMethod = this.data.rateWayArray[e.detail.value]
+			formData[e.currentTarget.dataset.rateMethod] = Number(e.detail.value) + 1
 			this.setData({
 				rateWayIndex: e.detail.value,
 				formData: formData
 			})
-		}
+		},
+
+		// 特殊条款
+		changeSpecificItems: function (e) {
+			let checkedValue = e.detail.value
+			let checkedItem = []
+			let checkedName = []
+			checkedValue.forEach((item, index) => {
+				checkedItem.push(this.data.specificItems.find((items) => { return items.name === item}).value)
+				checkedName.push(item)
+			})
+
+			let formData = this.data.formData
+			formData[e.currentTarget.dataset.specificItems] = checkedName.join('|')
+			this.setData({
+				specificitemsValue: checkedItem.join('、'),
+				formData: formData
+			})
+		},
 	}
 })
---
+++ app/ui/bond-form-editor/select-config/select-config.js
@@ -0,0 +1,54 @@
+module.exports = {
+	listingSpot: {
+		label: '上市地点：',
+		items: [
+			// {name: '102', value: 'XXXX', checked: 'true'},
+			{name: '1', value: '银行间'},
+			{name: '2', value: '上交所'},
+			{name: '3', value: '深交所'},
+			{name: '4', value: '其他'}
+		]
+	},
+	enterpriseNature: {
+		label: '企业性质：',
+		items: ['中央国企', '地方国企', '公众企业', '民营企业', '三资企业']
+		// items: [
+		// 	{name: '1', value: '中央国企'},
+		// 	{name: '2', value: '地方国企'},
+		// 	{name: '3', value: '公众企业'},
+		// 	{name: '4', value: '民营企业'},
+		// 	{name: '5', value: '三资企业'}
+		// ]
+	},
+	bondType: {
+		label: '债券品种：',
+		items: [
+			{name: '101', value: 'NCD'},
+			// {name: '102', value: '政金债', checked: 'true'},
+			{name: '103', value: '商行债'},
+			{name: '104', value: '次级债'},
+			{name: '105', value: '其他金融债'},
+			{name: '106', value: 'ABS'},
+			{name: '107', value: '公司债'},
+			{name: '108', value: '小公募'},
+			{name: '109', value: 'SCP'},
+		]
+	},
+	issuanceMethod: {
+		label: '上市地点：',
+		items: ['簿记建档', '荷兰式', '混合式']
+	},
+	rateWay: {
+		label: '利率方式：',
+		items: ['利随本清', '固定利率', '浮动利率', '累积利率']
+	},
+	specificClause: {
+		label: '特殊条款：',
+		items: [
+			{name: '1', value: '调整票面'},
+			{name: '2', value: '赎回'},
+			{name: '3', value: '延期'},
+			{name: '4', value: '交叉违约'}
+		]
+	}
+}
