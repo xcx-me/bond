3afdb0c5e9ee9d1f6341f2aa4e88f3fea877cc34
Anthony Chen <flashcgd@sina.com>
2018-04-26 16:34:36 +0800

    Add operation successful page.

--- app/page/user-detail-submission-complete/user-detail-submission-complete.js
+++ app/page/user-detail-submission-complete/user-detail-submission-complete.js
@@ -1,12 +1,17 @@
+// app/page/user-detail-submission-complete/user-detail-submission-complete.js
 Page({
-  	doNext: function () {
-		wx.navigateBack()
-	},
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+  
   },
 
   /**
