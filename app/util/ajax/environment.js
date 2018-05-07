ce5f50092854c580284e72963874f07af04c5a8e
rita <hf@appletreetech>
2018-05-07 14:39:52 +0800

    update sale info editor

--- app/util/ajax/environment.js
+++ app/util/ajax/environment.js
@@ -14,20 +14,20 @@ const isLocalhost = true
 // 	cookie: 'appletree_key=79500dc9a6909644eb19634500aef558aa5068bd' // sunqing
 // }
 
-// const SERVER = {
-// 	domain: 'http://test.qtrade.com.cn',
-// 	cookie: 'appletree_key=1ffc0512f2e1538fe6b8ea3041454cb2a885b03b' //ritahuang
-// }
+const SERVER = {
+	domain: 'http://test.qtrade.com.cn',
+	cookie: 'appletree_key=1ffc0512f2e1538fe6b8ea3041454cb2a885b03b' //ritahuang
+}
 
 // const SERVER = {
 // 	domain: 'http://192.168.1.203',
 // 	cookie: 'appletree_key=0d9a11bc036bac8e5fed95c672e933a5641c541b'
 // }
 
-const SERVER = {
-	domain: 'http://test.qtrade.com.cn',
-	cookie: 'appletree_key=daaca481c4c9183d337a4acb462946414c27cbb8' // Anthony
-}
+// const SERVER = {
+// 	domain: 'http://test.qtrade.com.cn',
+// 	cookie: 'appletree_key=daaca481c4c9183d337a4acb462946414c27cbb8' // Anthony
+// }
 
 function withDomain (path, domain) {
 	if (path.indexOf('http://') === 0 || path.indexOf('https://') === 0) return path
